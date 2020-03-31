a455ef18987379165bcb8ad760efae3e7c7c7d1b
Franz Holzinger <holzinger@ttproducts.de>
2020-03-31 16:43:31 +0200

    next version 0.0.2

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,7 +8,7 @@ $EM_CONF[$_EXTKEY] = array(
     'title' => 'Font Library',
     'description' => 'This provides the PHP Font Lib (php-font-lib).',
     'category' => 'misc',
-    'version' => '0.0.1',
+    'version' => '0.0.2',
     'state' => 'stable',
     'uploadfolder' => 0,
     'createDirs' => '',
@@ -19,7 +19,7 @@ $EM_CONF[$_EXTKEY] = array(
     'constraints' => array(
         'depends' => array(
             'php' => '5.4.0-7.0.99',
-            'typo3' => '6.2.0-9.3.99'
+            'typo3' => '6.2.0-9.5.99'
         ),
         'conflicts' => array(
         ),
